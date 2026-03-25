3d7a3f2ef1eca70e37d41c0974eff054e0772575
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2026-03-25 10:41:13 -0500

    feat: add the services controllers without any businnes logic implentation

---
+++ proxy/src/main/java/co/edu/eci/arep/proxy/ProxyController.java
@@ -0,0 +1,10 @@
+package co.edu.eci.arep.proxy;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="*")
+public class ProxyController {
+
+}
