8e4316638da381571b896274ae1d9a3dd92868b1
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2026-03-25 11:24:38 -0500

    feat: add passive active algorithm for balancing and the callservice method for invocate get services

--- proxy/src/main/java/co/edu/eci/arep/proxy/ProxyController.java
+++ proxy/src/main/java/co/edu/eci/arep/proxy/ProxyController.java
@@ -1,6 +1,13 @@
 package co.edu.eci.arep.proxy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +17,38 @@ public class ProxyController {
     private final String instance1 = System.getenv("INSTANCE1_URL") != null ? System.getenv("INSTANCE1_URL") : System.getenv("http://:localhost:8081");
     private final String instance2 = System.getenv("INSTANCE2_URL") != null ? System.getenv("INSTANCE2_URL") : System.getenv("http://:localhost:8082");
 
+     @GetMapping("/proxy/function1")
+    public String function1(@RequestParam String value) {
+        return delegate("/function1?value=" + value);
+    }
+
+    // active passive
+    private String delegate(String path) {
+        try {
+            return callService(instance1 + path);
+        } catch (Exception e) {
+            try {
+                return callService(instance2 + path);
+            } catch (Exception e2) {
+                return "{\"error\":\"all instances down\"}";
+            }
+        }
+    }
+
+    private String callService(String urlStr) throws Exception {
+        URL url = new URL(urlStr);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setConnectTimeout(3000);
+        con.setReadTimeout(3000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = in.readLine()) != null) response.append(line);
+        in.close();
+        return response.toString();
+    }
+
+
 }
+
