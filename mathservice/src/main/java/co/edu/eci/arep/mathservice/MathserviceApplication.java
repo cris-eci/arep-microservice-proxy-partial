85682e0d26c1901fbbfd5770a80b5dd0d37140d5
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2026-03-25 10:25:33 -0500

    feat: add mathservice spring boot initializer

---
+++ mathservice/src/main/java/co/edu/eci/arep/mathservice/MathserviceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eci.arep.mathservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathserviceApplication.class, args);
+	}
+
+}
---
+++ mathservice/src/test/java/co/edu/eci/arep/mathservice/MathserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.arep.mathservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
