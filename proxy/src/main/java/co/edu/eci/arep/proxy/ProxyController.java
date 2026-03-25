40d04019b7ee7f8bef3fe1de6fdea8d3a8e9fc77
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2026-03-25 11:17:47 -0500

    feat: add getenv variables for OS instances in proxy

--- proxy/src/main/java/co/edu/eci/arep/proxy/ProxyController.java
+++ proxy/src/main/java/co/edu/eci/arep/proxy/ProxyController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins="*")
 public class ProxyController {
 
+    private final String instance1 = System.getenv("INSTANCE1_URL") != null ? System.getenv("INSTANCE1_URL") : System.getenv("http://:localhost:8081");
+    private final String instance2 = System.getenv("INSTANCE2_URL") != null ? System.getenv("INSTANCE2_URL") : System.getenv("http://:localhost:8082");
+
 }
